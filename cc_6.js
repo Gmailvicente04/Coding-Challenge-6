71b15b2d07941fed6e5f2a44d7f464a02f425636
Gmailvicente04 <vicente.oswald@gmail.com>
2025-02-09 17:18:06 -0500

    "Task 3 - Employee Bonus Calculation."

--- cc_6.js
+++ cc_6.js
@@ -16,5 +16,24 @@ const calculateSalesTax = function(amount, taxRate) {
   console.log(calculateSalesTax(100, 0.07)); // Expected output: "Sales Tax: $7"
   console.log(calculateSalesTax(500, 0.1));  // Expected output: "Sales Tax: $50"
 
+  // Task 3 - Employee Bonus Calculation
+const calculateBonus = (salary, performanceRating) => {
+    let bonusPercentage;
+    if (performanceRating === "Excellent") {
+      bonusPercentage = 0.2;
+    } else if (performanceRating === "Good") {
+      bonusPercentage = 0.1;
+    } else if (performanceRating === "Average") {
+      bonusPercentage = 0.05;
+    } else {
+      bonusPercentage = 0;
+    }
+    let bonus = salary * bonusPercentage;
+    return `Bonus: $${bonus}`;
+  }
+  
+  console.log(calculateBonus(5000, "Excellent")); // Expected output: "Bonus: $1000"
+  console.log(calculateBonus(7000, "Good"));      // Expected output: "Bonus: $700"
+
   
 
