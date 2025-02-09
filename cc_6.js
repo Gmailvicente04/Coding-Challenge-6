e48afb42b32d7a5c5164d8ec72378e1234641c72
Gmailvicente04 <vicente.oswald@gmail.com>
2025-02-09 17:16:19 -0500

    "Task 2 - Sales Tax Computation."

--- cc_6.js
+++ cc_6.js
@@ -7,5 +7,14 @@ function calculateProfit(costPrice, sellingPrice, unitsSold) {
   console.log(calculateProfit(20, 30, 100)); // Expected output: "Total Profit: $1000"
   console.log(calculateProfit(50, 70, 200)); // Expected output: "Total Profit: $4000"
 
+  // Task 2 - Sales Tax Computation
+const calculateSalesTax = function(amount, taxRate) {
+    let salesTax = amount * taxRate;
+    return `Sales Tax: $${salesTax}`;
+  }
+  
+  console.log(calculateSalesTax(100, 0.07)); // Expected output: "Sales Tax: $7"
+  console.log(calculateSalesTax(500, 0.1));  // Expected output: "Sales Tax: $50"
+
   
 
