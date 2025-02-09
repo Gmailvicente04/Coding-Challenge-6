20e31cf16bea9d836c52acc7de9858d284803f9e
Gmailvicente04 <vicente.oswald@gmail.com>
2025-02-09 17:15:38 -0500

    "Task 1 - Business Profit Calculation."

---
+++ cc_6.js
@@ -0,0 +1,11 @@
+// Task 1 - Business Profit Calculation
+function calculateProfit(costPrice, sellingPrice, unitsSold) {
+    let profit = (sellingPrice - costPrice) * unitsSold;
+    return `Total Profit: $${profit}`;
+  }
+  
+  console.log(calculateProfit(20, 30, 100)); // Expected output: "Total Profit: $1000"
+  console.log(calculateProfit(50, 70, 200)); // Expected output: "Total Profit: $4000"
+
+  
+
